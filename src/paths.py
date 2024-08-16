a880144d1dc32439702fd4184dcc3c3ac094d10b
rt-desai <abhyuday.desai@readytensor.com>
2024-08-15 17:50:02 -0700

    rename config files

--- src/paths.py
+++ src/paths.py
@@ -6,6 +6,6 @@ DATA_DIR_PATH = os.path.join(ROOT_DIR, "data")
 
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 
-TSP_CONFIG_FILE_PATH = os.path.join(SRC_DIR, "config", "tsp_config.json")
+TSP_CONFIG_FILE_PATH = os.path.join(SRC_DIR, "config", "tsp_scenarios.json")
 
-GEN_CONFIG_FILE_PATH = os.path.join(SRC_DIR, "config", "gen_config.json")
+GEN_CONFIG_FILE_PATH = os.path.join(SRC_DIR, "config", "dataset_gen_config.json")
